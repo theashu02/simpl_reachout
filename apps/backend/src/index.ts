c3d95d1822bfb30fffbd89dc0ca64cb719e24f56
theashu02 <ashutoshchauhan1919@gmail.com>
2025-12-25 17:20:20 +0530

    fixes

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -74,6 +74,6 @@ app.use(fileTransferWs);
 app.listen(PORT);
 
 if (app.server) {
-  console.log(`🦊 HTTP server running at http://${app.server.hostname}:${app.server.port}`);
-  console.log(`📡 File-transfer signaling ready at ws://${app.server.hostname}:${app.server.port}/ws`);
+  console.log(`HTTP server running at http://${app.server.hostname}:${app.server.port}`);
+  console.log(`File-transfer signaling at ready ws://${app.server.hostname}:${app.server.port}/ws`);
 }
